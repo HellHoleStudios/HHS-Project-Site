7c157a26acab07d065d317b039aee23ac7752c20
XiaoGeNintendo <34835642+XiaoGeNintendo@users.noreply.github.com>
2019-08-13 11:32:20 +0800

    member data update

--- member.js
+++ member.js
@@ -5,9 +5,23 @@ function loadMemberData(){
     root.innerHTML="";
     for(var i=0;i<member.length;i++){
         x+='<div class="card"><div class="card-body"><table width="80%"><tr><td width="50%">';
-        x+='<img src="'+member[i]["pic"]+'" alt="'+member[i]["name"]+'"/></td>';
-        x+='<td width="50%"><b>'+member[i]["name"]+'</b><br/><i>'+member[i]["real"]+'</i><br/>Role:<span class="'+role[member[i]["rank"]]["code"]+'">'+role[member[i]["rank"]]["display"]+'</span><br/>';
-        x+="aka:"+member[i]["aka"];
+        x+='<img width=100px height=100px src="'+member[i]["pic"]+'" alt="'+member[i]["name"]+'"/></td>';
+        x+='<td width="50%"><b>'+member[i]["name"]+'</b><br/><i>'+member[i]["real"]+'</i><br/>Role:';
+        x+='<span class="'+role[member[i]["rank"]]["code"]+'">'+role[member[i]["rank"]]["display"]+'</span>';
+        if(member[i]["skype"]){
+            x+=' <span class="badge badge-warning">In Skype</span>';
+        }
+        if(member[i]["discord"]){
+            x+=' <span class="badge badge-danger">In Discord</span>';
+        }
+        x+="</br>"
+
+        x+="aka:"+member[i]["aka"]+"<br/>";
+        x+="Contact:";
+        for(var j=0;j<member[i]["contact"].length;j++){
+            x+='<a href="'+member[i]["contact"][j]["link"]+'">'+member[i]["contact"][j]["display"]+"</a>";
+            x+=" ";
+        }
         x+="</td></tr></table></div></div>";
         x+="<br/>"
     }
--- memberdata.js
+++ memberdata.js
@@ -4,16 +4,147 @@ var member=[
         "real":"Jiongjie Hua",
         "rank":"core",
         "pic":"",
-        "aka":"HDD,HJJ"
+        "skype":true,
+        "discord":true,
+        "aka":"HDD,HJJ",
+        "contact":[
+        ]
     },
     {
         "name":"Zzzyt",
         "real":"Yuqi Zhao",
         "rank":"core",
+        "skype":true,
+        "discord":true,
+        "pic":"https://userpic.codeforces.com/594107/title/bb5fc2110e40d3ef.jpg",
+        "aka":"XiaoMao205,XiaoMao,Zeke,Son,ZJS",
+        "contact":[
+            {
+                "link":"https://codeforces.com/profile/zzzyt",
+                "display":"Codeforces"
+            },
+            {
+                "link":"https://github.com/Zzzzzzyt",
+                "display":"Github"
+            }
+        ]
+    },
+    {
+        "name":"XGN",
+        "real":"Wenqing Ge",
+        "rank":"core",
+        "skype":true,
+        "discord":true,
+        "pic":"https://avatars2.githubusercontent.com/u/34835642?s=460&v=4",
+        "aka":"XiaoGeNintendo,gwq2017",
+        "contact":[
+            {
+                "link":"https://codeforces.com/profile/gwq2017",
+                "display":"Codeforces"
+            },
+            {
+                "link":"https://codeforces.com/profile/I_love_hellholestudios",
+                "display":"Codeforces2"
+            },
+            {
+                "link":"https://github.com/XiaoGeNintendo",
+                "display":"Github"
+            }
+        ]
+    },
+    {
+        "name":"XIZCM",
+        "real":"Ziming Xu",
+        "rank":"core",
+        "skype":true,
+        "discord":false,
+        "pic":"https://avatars2.githubusercontent.com/u/44310508?s=460&v=4",
+        "aka":"IC,XZM",
+        "contact":[
+            {
+                "link":"https://codeforces.com/profile/XIZCM",
+                "display":"Codeforces"
+            },
+            {
+                "link":"https://github.com/DamnXIZCM",
+                "display":"Github"
+            }
+        ]
+    },
+    {
+        "name":"ZHD",
+        "real":"Huairui Zhang",
+        "rank":"main",
+        "skype":true,
+        "discord":false,
+        "pic":"https://userpic.codeforces.com/221895/title/8b7dd929462125f.jpg",
+        "aka":"ZYD,ZHR,ZHDssss",
+        "contact":[
+            {
+                "link":"https://codeforces.com/profile/bp_ZHD_qd",
+                "display":"Codeforces"
+            }
+        ]
+    },
+    {
+        "name":"MonkeyKing",
+        "real":"Guohao Jing",
+        "rank":"related",
+        "skype":true,
+        "discord":false,
+        "pic":"https://userpic.codeforces.com/585307/title/fffec0879f92df58.jpg",
+        "aka":"MK,Orange Boy",
+        "contact":[
+            {
+                "link":"https://codeforces.com/profile/MonkeyKing",
+                "display":"Codeforces"
+            }
+        ]
+    },
+    {
+        "name":"ZhuKy.RLG",
+        "real":"Kunyuan Zhu",
+        "rank":"main",
+        "skype":true,
+        "discord":false,
+        "pic":"https://userpic.codeforces.com/322867/title/72d4acfb76302ada.jpg",
+        "aka":"Zhuky,ZKY",
+        "contact":[
+            {
+                "link":"https://codeforces.com/profile/ZhuKY.RLG",
+                "display":"Codeforces"
+            }
+        ]
+    },
+    {
+        "name":"WZZ",
+        "real":"Zizhou Wang",
+        "rank":"main",
+        "skype":true,
+        "discord":false,
         "pic":"",
-        "aka":"XiaoMao205,XiaoMao,Zeke,Son"
-    }
+        "aka":"WN",
+        "contact":[
+            {
+                "link":"https://codeforces.com/profile/Hytedresorale_Toad",
+                "display":"Codeforces"
+            }
+        ]
+    },
+    {
+        "name":"HYK",
+        "real":"Yunke Hu",
+        "rank":"branch",
+        "skype":true,
+        "discord":false,
+        "pic":"",
+        "aka":"",
+        "contact":[
+            
+        ]
+    },
 ]
+
 var role={
     "core":{
         "code":"badge badge-success",
