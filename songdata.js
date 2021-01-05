d64f45a0bee7bd429df7da430f357e06d145a991
XiaoGeNintendo <34835642+XiaoGeNintendo@users.noreply.github.com>
2021-01-05 22:01:41 +0800

    update video info

--- songdata.js
+++ songdata.js
@@ -5,16 +5,31 @@ var __song=[
         "release": "2018-07-29",
         "comment": "NSFW!!",
         "tag": ["Video","Kdenlive","ENG"],
-        "length": "??:??", //please strict follow mm:ss
+        "length": "01:34", //please strict follow mm:ss
         "source": "更衣室大战"
     },
     {
         "name": "五谷轮回之力(The power of five-corns-going-round-and-round)",
         "author": ["Zzzyt"],
         "release": "2017-01-30",
-        "tag": [
-        "Video","Mine-Imator","ENG","CHN"],
+        "tag": ["Video","Mine-Imator","ENG","CHN"],
         "length":"??:??",
         "source": "Original"
+    },
+    {
+        "name": "Tribute!",
+        "author": ["HDD"],
+        "release": "2018-03-23",
+        "tag": ["Video"],
+        "length":"00:50",
+        "source": "?"
+    },
+    {
+        "name": "Pike Place",
+        "author": ["HDD"],
+        "release": "2018-02-12",
+        "tag": ["Video"],
+        "length":"00:40",
+        "source": "?"
     },
 ]
